67e9038ecb223c7ee0988ecf68ba14f510b29464
Anthony Lombardi <ajlombardi5@gmail.com>
2023-12-27 13:26:00 -0500

    Add buffer switch mappings

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -5,6 +5,8 @@ M.general = {
     ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>", "window right"},
     ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>", "window down"},
     ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>", "window up"},
+    ["<A-,>"] = {"<cmd> bp<CR>", "Previous buffer"},
+    ["<A-.>"] = {"<cmd> bn<CR>", "Next buffer"},
   }
 }
 M.dap = {
