00da57fabe963593788462b7032e7f6b422c1334
Anthony Lombardi <ajlombardi5@gmail.com>
2024-01-11 17:26:19 -0500

    Add TPM to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 echo "Installing dependencies"
 sudo apt install zsh tmux python3 python3-venv
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Linking zsh"
 ln -s $(pwd)/zshrc ~/.zshrc
 echo "Linking tmux"
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
+echo "Use <prefix>+I to install tmux plugins on first launch"
 echo "Linking nvim"
 ln -s $(pwd)/nvim/ ~/.config/nvim
 echo "Setting nvim as default git editor"
