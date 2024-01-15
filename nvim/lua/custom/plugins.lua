6a8fd0f396fa9a029d9daed1cbfb6d696a80c62b
Anthony Lombardi <ajlombardi5@gmail.com>
2024-01-15 12:32:07 -0500

    Add rust-lang support

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,5 +1,23 @@
 local plugins = {
   {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
@@ -77,6 +95,7 @@ local plugins = {
         "black",
         "isort",
         "debugpy",
+        "rust-analyzer"
       }
     }
   }
